24aa2470647ab8b9b8c6c91a9294043449320b99
Malcolm Mason <79657911+malmason@users.noreply.github.com>
2021-08-25 14:01:59 -0400

    added .gitignore

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,7 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
 
-const dbKey = process.env.DB_LIVEKEY;
+const dbKey = process.env.DB_KEY;
 const stripe = require('stripe')(`${dbKey}`);
 
 const resolvers = {
